22c151ff3612d4dbf7a96252ff7ba095bcb6b449
Liam <liamsalass@yahoo.com>
2022-06-14 15:12:12 -0400

    Figured out how srand and rand functions work.

--- BlackJack/BlackJack.cpp
+++ BlackJack/BlackJack.cpp
@@ -1,20 +1,29 @@
-// BlackJack.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+#include <array>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool cont = true;
+    cout << "Welcome to BlackJack!\nTo win points you must draw cards until you have a sum of 21.\nIf you draw over 21, you lose.\nIf you draw cards less than or equal to the dealer, you also lose." << endl;
+    srand(time(NULL));
+    while (cont) {
+        int card1 = rand() % 13 + 1;
+        int card2 = rand() % 13 + 1;
+        cout << "Card 1 is: " << card1 << ", and card 2 is: " << card2 <<  endl;
+        
+
+
+
+        cont = false;
+    };
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
